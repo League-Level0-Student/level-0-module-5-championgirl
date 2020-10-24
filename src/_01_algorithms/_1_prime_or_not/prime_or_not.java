8edd67519aec9a2c2f79bc43dd0912a4303de662
championgirl <66965981+championgirl@users.noreply.github.com>
2020-10-24 14:26:17 -0700

    prime or not!

---
+++ src/_01_algorithms/_1_prime_or_not/prime_or_not.java
@@ -0,0 +1,26 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class prime_or_not {
+public static void main(String[] args) {
+	
+
+String number = JOptionPane.showInputDialog("Type a number!");
+
+for(int i = 0; i < ; i++ ) {
+	
+	
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+}
+	
+}
