6d2be20fed1d146669c65c7fc3e3822d6d1b7f51
root <root@94f89bee451c>
2020-12-10 01:10:04 +0000

    nested forloops WIP

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
@@ -0,0 +1,96 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+import javax.swing.JOptionPane;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		
+	//	for(int i = 0; i<101; i++) {
+			
+		//	System.out.println(i);
+			
+		//}
+		
+		
+		//for(int i = 100; i>-1; i--) {
+			//System.out.println(i);
+		
+		
+		
+		//}
+		
+	
+	
+	
+	
+	
+	
+	
+		//for(int i = 2; i<101; i++) {
+			
+	//if(i%2==0){
+		 
+		//System.out.println(i);
+	//}
+		
+	//	}
+		//for(int i = 1; i<101; i++) {
+			
+			//if(i%2==1){
+				 
+
+			//		//System.out.println(i);
+			//}
+	
+		//}
+		
+		
+		
+		
+for(int i = 1; i<500; i++) {
+			
+			if(i%2==1){
+		
+				System.out.println(i+ " is odd");
+				
+				
+				
+			}
+			
+			
+			if(i%2==0) {
+				
+				System.out.println(i+" is even");
+		
+				
+				
+				
+			}
+	
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+	
+		
+	}
+	
+	
+	
+
