ea6265bdc8371c8c1544f790239da272a888bc0a
root <root@e4fc757cb1ec>
2020-12-11 01:02:32 +0000

    forloops

--- src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoops.java
@@ -48,44 +48,84 @@ public class NestedLoops {
 		
 		
 		
-for(int i = 1; i<500; i++) {
+//for(int i = 1; i<500; i++) {
 			
-			if(i%2==1){
+		//	if(i%2==1){
 		
-				System.out.println(i+ " is odd");
+			//	System.out.println(i+ " is odd");
 				
 				
 				
-			}
+			//}
 			
 			
-			if(i%2==0) {
+		//	if(i%2==0) {
 				
-				System.out.println(i+" is even");
+			//	System.out.println(i+" is even");
 		
 				
 				
 				
-			}
+		//	}
 	
-		}
+		//}
+		          
+		
+		
+		
+//for(int i = 0;i<=777 ; i+=7) {
+			
+		
+				 
+
+			//System.out.println(i);
+		//}
+		
+		
+	
+		
+	//	
+		//for(int i = 0; i<=9; i++) {
+			
+				
+			//for(int j = 0; j<=3; j++) {
+			//System.out.println(i);
+			
+			
+			//}
+				
+			//for(int x = 0; x<=3; x++) {
+				//System.out.println(i);
+			///}
+			//
+		
+		//}
+		
+		
+	//	for(int i = 0; i<=9; i++) {
+		//int j=i;j++;
+		
+		
+	//if(j>=10) {
+		//j=0;
+	//}
+				
+			//System.out.println(i+" "+i+" "+i );
+		///	System.out.print(i+" ");
+		//	System.out.print(i+" ");
+		//	
+			
+			//}
+				
+
 		
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+	
+
+
+}
 		
 	
 		
