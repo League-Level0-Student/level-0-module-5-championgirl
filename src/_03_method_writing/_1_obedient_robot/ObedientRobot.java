66c79304408fc9f73c4e141f5f9a1ed2ee365563
championgirl <66965981+championgirl@users.noreply.github.com>
2021-01-28 16:56:19 -0800

    ObedientRobotWIP

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -1,34 +1,57 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-
+	static Robot coder = new Robot("mini");
+	
 public static void main(String[] args) {
 
-	Robot coder = new Robot();
-	
-	
+	coder.penDown();  
+	coder.setSpeed(5);
 
-	
+	circle();
+}
 
-coder.penDown();
-  
-coder.setSpeed(5);
+
+
+static void square () {
 
 coder.move(10); 
-
 coder.turn(90);
-
 for( int i = 0; i <4; i++ ) {
 coder.move(80);
 coder.turn(90);
 }
+}
 
+
+static void triangle () { 
+	
+for( int i = 0; i <3; i++ ) {
+coder.move(80);
+coder.turn(125);
+}
+}
+
+
+static void circle () {
+
+coder.turn(360);
+for(int i = 0; i<360; i++) {
+coder.move(3);
+coder.turn(1);
+ }
+ }
+
+String shape = JOptionPane.showInputDialog("What shape do you want");
+
+(if shape = "square"){
+	square();
+}
 
 
 
 }
-
-}
-
