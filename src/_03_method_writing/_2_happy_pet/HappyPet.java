261f1992730945644dea4640443d195280bb7062
championgirl <66965981+championgirl@users.noreply.github.com>
2021-02-11 17:02:05 -0800

    HappyPetWIP

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -6,17 +6,25 @@ public class HappyPet {
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
 	
+	static int happinessLevel = 0;
+	
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 
+		String pet = JOptionPane.showInputDialog("What kind of pet do you want to buy?");
+		
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+		
+		  
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy", "Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Cuddle", "Give food", "Take a walk" }, null);
+   if(task==0) {
+	   Cuddle(pet);
+   }
 			// 5. Use user input to call the appropriate method created in step 4.
 
 			// 6. If you determine the happiness level is large enough, tell the
@@ -24,7 +32,99 @@ public class HappyPet {
 
 	}
 
+
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	
+	
+	static void Cuddle(String pet) {
+		
+		happinessLevel+=45;
+	
+		PetFeelings(pet);
+		
+		
+	}
+	
+	static void GiveFood(String pet) {
+		
+		happinessLevel+=35;
+	
+		PetFeelings(pet);
+		
+		
+	}
+static void TakeAWalk(String pet) {
+		
+		happinessLevel+=35;
+	
+		PetFeelings(pet);
+		
+		
+	}
+	
+	
+	
+	static void PetFeelings(String pet) {
+	
+	if( pet.contentEquals("cat")){
+		
+	JOptionPane.showMessageDialog(null,"I feel Purrfect! 😸");
+
+}
+
+if( pet.contentEquals("dog")){
+	
+	JOptionPane.showMessageDialog(null,"I feel Pawsome 🐕 ! ");
+
+}
+
+
+
+if( pet.contentEquals("fish")){
+	
+	JOptionPane.showMessageDialog(null," I feel fintastic! ");
+
+
+}
+
+if( pet.contentEquals("Lion")){
+	
+	JOptionPane.showMessageDialog(null," ROAR I FEEL GREAT! ");
+
+
+}
+
+if( pet.contentEquals("Snake")){
+	
+	JOptionPane.showMessageDialog(null," I feel Ssssimply SsssThe bestssssssssssssss");
+
+
+}
+
+if( pet.contentEquals("Red Panda")){
+	
+	JOptionPane.showMessageDialog(null,"I'M ENDANGERED YOU ARE NOT ALLOWED TO KEEP ME AS A PET!!!!! Other than that I feel GREAT! :D");
+
+
+}
+
+if( pet.contentEquals(" ")){
+	
+	JOptionPane.showMessageDialog(null,"You did not pick an animal please try again.");
+
+
+}
+
+else {
+	JOptionPane.showMessageDialog(null,"We do not have that animal yet please choose another pet.");
+
+}
+
+
+
+	}
+	
+	
 }
