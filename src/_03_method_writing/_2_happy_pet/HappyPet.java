01b03f54775d894f73157abf4e81ff01d20dc3cc
championgirl <66965981+championgirl@users.noreply.github.com>
2021-02-18 16:55:41 -0800

    HappyPetFinshed,Skill Practice WIP

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -1,130 +1,139 @@
 package _03_method_writing._2_happy_pet;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+
+	// 2. Add the following variable to the next line: static int happinessLevel =
+	// 0;
 	// this will be used to store the happiness of your pet
-	
+
 	static int happinessLevel = 0;
-	
+
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
 
 		String pet = JOptionPane.showInputDialog("What kind of pet do you want to buy?");
-		
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-		
-		  
-			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy", "Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Cuddle", "Give food", "Take a walk" }, null);
-   if(task==0) {
-	   Cuddle(pet);
-   }
-			// 5. Use user input to call the appropriate method created in step 4.
 
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+		if (pet.contentEquals("")) {
+
+			JOptionPane.showMessageDialog(null, "You did not pick an animal please try again.");
+
+		}
+
+		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+
+		// 3. Use showOptionDialog to ask the user what they want to do to make their
+		// pet happy
+		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+		// Make sure to customize the title and question too.
+while (happinessLevel < 20) {
+	
+
+		int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy", "Pet", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Cuddle", "food", "walk" }, null);
+		if (task == 0) {
+			Cuddle(pet);
+			food(pet);
+			walk(pet);
+		}
+}
+		// 5. Use user input to call the appropriate method created in step 4.
+
+		// 6. If you determine the happiness level is large enough, tell the
+		// user that he loves his pet and use break; to exit for loop.
 
 	}
-
 
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
-	
-	
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
+
+	static void food(String pet) {
+
+		happinessLevel += 5;
+
+		PetFeelings(pet);
+
+	}
+
 	static void Cuddle(String pet) {
-		
-		happinessLevel+=45;
-	
+
+		happinessLevel += 3;
+
 		PetFeelings(pet);
-		
-		
+
 	}
-	
-	static void GiveFood(String pet) {
-		
-		happinessLevel+=35;
-	
+
+	static void walk(String pet) {
+
+		happinessLevel += 7;
+
 		PetFeelings(pet);
-		
-		
+
 	}
-static void TakeAWalk(String pet) {
-		
-		happinessLevel+=35;
-	
-		PetFeelings(pet);
-		
-		
-	}
-	
-	
-	
+
 	static void PetFeelings(String pet) {
-	
-	if( pet.contentEquals("cat")){
-		
-	JOptionPane.showMessageDialog(null,"I feel Purrfect! 😸");
 
-}
+		if (pet.contentEquals("cat")) {
 
-if( pet.contentEquals("dog")){
-	
-	JOptionPane.showMessageDialog(null,"I feel Pawsome 🐕 ! ");
+			JOptionPane.showMessageDialog(null, "I feel Purrfect! 😸");
 
-}
+		}
 
+		if (pet.contentEquals("dog")) {
 
+			JOptionPane.showMessageDialog(null, "I feel Pawsome 🐕 ! ");
 
-if( pet.contentEquals("fish")){
-	
-	JOptionPane.showMessageDialog(null," I feel fintastic! ");
+		}
 
+		if (pet.contentEquals("fish")) {
 
-}
+			JOptionPane.showMessageDialog(null, " I feel fintastic! ");
 
-if( pet.contentEquals("Lion")){
-	
-	JOptionPane.showMessageDialog(null," ROAR I FEEL GREAT! ");
+		}
 
+		if (pet.contentEquals("lion")) {
 
-}
+			JOptionPane.showMessageDialog(null, " ROAR I FEEL GREAT! ");
 
-if( pet.contentEquals("Snake")){
-	
-	JOptionPane.showMessageDialog(null," I feel Ssssimply SsssThe bestssssssssssssss");
+		}
 
+		if (pet.contentEquals("snake")) {
 
-}
+			JOptionPane.showMessageDialog(null, " I feel Ssssimply the bestssssssssssssss");
 
-if( pet.contentEquals("Red Panda")){
-	
-	JOptionPane.showMessageDialog(null,"I'M ENDANGERED YOU ARE NOT ALLOWED TO KEEP ME AS A PET!!!!! Other than that I feel GREAT! :D");
+		}
 
+		if (pet.contentEquals("red panda")) {
 
-}
+			JOptionPane.showMessageDialog
+			(null,"I'M ENDANGERED YOU ARE NOT ALLOWED TO KEEP ME AS A PET!!!!! Other than that I feel GREAT! :D");
 
-if( pet.contentEquals(" ")){
-	
-	JOptionPane.showMessageDialog(null,"You did not pick an animal please try again.");
+		}
+		if (pet.contentEquals("bunny")) {
 
+			JOptionPane.showMessageDialog(null,"You make me HOPPY!🐇  :) ");
 
-}
+		}
+		if (pet.contentEquals("dragon")) {
 
-else {
-	JOptionPane.showMessageDialog(null,"We do not have that animal yet please choose another pet.");
+			JOptionPane.showMessageDialog(null," I feel dragontastic! ");
 
-}
+		}
+		if (pet.contentEquals("lizard")) {
 
+			JOptionPane.showMessageDialog(null," I feel awesome! ");
 
+		}
+		if (pet.contentEquals("panda")) {
+
+			JOptionPane.showMessageDialog(null,	"  I feel awesome ");
+
+		}
 
 	}
-	
-	
+
 }
---
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -0,0 +1,48 @@
+package _04_practice._1_skill_practice;
+
+import javax.swing.JOptionPane;
+
+public class SkillPractice {
+	public static void main(String[] args) {
+
+		
+		// CONVERT DIMES INTO CENTS//
+
+//String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+
+//int numberdimes = Integer.parseInt(dimes);
+
+//int cents = numberdimes * 10;
+
+		// JOptionPane.showMessageDialog(null, cents+" cents");
+
+		
+		
+		// HEIGHT TEST//
+
+// String height = JOptionPane.showInputDialog("How tall are you (in inches)");
+
+// int inches = Integer.parseInt(height);
+
+// if (inches<36) {
+
+// JOptionPane.showMessageDialog(null,"Eat your Wheaties.");
+
+// }
+
+// else{
+// JOptionPane.showMessageDialog(null, "Good job eating your Wheaties!");
+// }
+
+		
+		
+		// WONDERFULL SCHOOL//
+
+		// String School = JOptionPane.showInputDialog("Whats the name of your
+		// school?");
+
+		// JOptionPane.showMessageDialog(null, School +" is a wonderful school and you
+		// are too!");
+
+	}
+}
